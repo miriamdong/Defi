2e6903bce3e75b23003eb0aaa7e6d36d716575c4
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-06 22:08:50 -0700

    some more user routes and readme.mda

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+## User Routes
+<table border="1">
+<tr><td>Get user By id</td><td>/users/:id</td></tr>
+<tr><td>Get user By auth id</td><td>/users/auth/:id</td></tr>
+</table>
--- backend/db/queries/users.js
+++ backend/db/queries/users.js
@@ -1,12 +1,17 @@
 const knex = require('../../knexfile');
 
 const getUserById = (id) => {
-  console.log(id)
     return knex('users')
         .where('id', id)
         .select('*');
 }
 
+const getUserByAuthId = (authId) => {
+    return knex('users')
+        .where('auth_id', authId)
+        .select('*');
+}
+
 module.exports = {
-  getUserById
+  getUserById, getUserByAuthId
 }
