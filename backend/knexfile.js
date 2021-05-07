aec8e59eba8f41a4cb16973d805d2be1429d2564
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-07 09:22:43 -0700

    picking DB url from env file

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = require('knex')({
   client: 'pg',
-  connection: 'postgres://labber:labber@localhost:5432/defi'
+  connection: process.env.DB_URL
 });
