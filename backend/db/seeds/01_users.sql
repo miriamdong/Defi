5228c1b1a5b1adebc512a165c6e212351653f520
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-07 12:51:09 -0700

    removing backend

--- backend/db/seeds/01_users.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (auth_id, name) VALUES ('111111', 'Erik Kiser');
-INSERT INTO users (auth_id, name) VALUES ('222222', 'William Kragie');
-INSERT INTO users (auth_id, name) VALUES ('333333', 'John Hagler');
-INSERT INTO users (auth_id, name) VALUES ('444444', 'Eric Pronovost');
--- backend/db/seeds/05_images.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO images (project_id, image)
-VALUES(1, pg_read_binary_file('/Users/kapilgarg/lighthouse/Defi/backend/db/images/cocktail.jpeg'));
-INSERT INTO images (project_id, image)
-VALUES(2, pg_read_binary_file('/Users/kapilgarg/lighthouse/Defi/backend/db/images/Husky.jpeg'));
