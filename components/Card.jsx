fe4bad7480530236b1be4e60329707a496695c44
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-09 11:28:00 -0700

    resoling conflict

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,21 @@
 /* This example requires Tailwind CSS v2.0+ */
 import Link from 'next/link';
 import useAppData from "../hooks/useAppData.js";
+import { NewspaperIcon, PhoneIcon, SupportIcon } from "@heroicons/react/outline";
+import FirebaseAuth from "./Auth/FirebaseAuth";
+import React, { useRef, useState } from "react";
+import { firebaseClient } from "../firebase/initFirebase";
+import { useUser } from "../firebase/useUser";
+import { Popover, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import Sign_in from './Sign_in';
 
 export default function Card() {
 
   const {state} = useAppData();
-
+  const { user, logout } = useUser();
+  const [isShown,setIsShown] = useState(false)
+  const [toggle, setToggle] =useState(false)
   return (
     <section
       className=" -mt-32 max-w-7xl mx-auto relative z-10 pb-32 px-4 sm:px-6 lg:px-8  "
@@ -33,24 +43,41 @@ export default function Card() {
           </div>
 
         ))}
-        <Link href="/project/create">
-        <div className="flex flex-col bg-purple-900 rounded-2xl shadow-xl ">
-
+        {user ? (
+          <Link href="/project/create">
+            <div className="flex flex-col bg-purple-900 rounded-2xl shadow-xl ">
+              <div className="flex-1 relative ">
+                <div className=" px-4 py-10 sm:px-6 text-white">
+                  <h3>CREATE NEW PROJECT</h3>
+                </div>
+                <div>
+                  <img src="./img/min.gif" alt="New Project" />
+                </div>
+              </div>
+            </div>
+          </Link>
+        ) : (
+          <div className="flex flex-col bg-purple-900 rounded-2xl shadow-xl " onClick={()=>{
+            const abc = document.getElementsByClassName("sign-in-button")
+            abc[0].click()
+            setToggle((prev)=>{
+              return !prev
+            })
+          }}>
             <div className="flex-1 relative ">
-              <div className=" px-4 py-10 sm:px-6 text-white">
-                <h3>CREATE NEW PROJECT</h3>
-
+              <div className=" px-4 py-10 sm:px-6 text-white" style={{textAlign: "center"}} >
+                {!toggle ? 
+                <h3>CREATE NEW PROJECT</h3>: <h3>PLEASE LOGIN FIRST!!!</h3>}
               </div>
-              <div  ><img
-                    src="./img/min.gif"
-                    alt="New Project"
-              /></div>
+              <div>
+                <img src="./img/min.gif" alt="New Project" />
               </div>
+            </div>
 
-      </div>
-       </Link>
-      </div>
+          </div>
 
+        )}
+      </div>
     </section>
   );
 }
