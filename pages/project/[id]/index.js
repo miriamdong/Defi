3e4cc6939dd04df0a48c52e0203f8e51986bfacf
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-10 10:34:58 -0700

    adding comments

--- pages/project/[id]/index.js
+++ pages/project/[id]/index.js
@@ -2,9 +2,23 @@ import axios from 'axios';
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 import {useEffect, useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 const Project = () => {
+  const classes = useStyles();
   const router = useRouter();
+  
   const [project, setProject] = useState({
     name: "",
     description: "",
@@ -36,6 +50,12 @@ const Project = () => {
         {project.round}<br/>
         {project.contract}<br/>
         <img src={project.image} width="300"/>
+        
+        <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <Button type="submit">Comment</Button>
+    </form>
+
         <Link href="/">
         <button>Go Back</button>
         </Link>
