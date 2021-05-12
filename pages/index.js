bd6316ea838f0953d17b639be7b98c0ffdfee90f
Miriam Dong <miriamdong@gmail.com>
2021-05-11 19:49:38 -0700

    reslove conflits

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,7 @@ export default function Home() {
   const { user, logout } = useUser();
   return (
     <div>
-      <Head>
-        <title>rocketMEOW</title>
-        <link rel="icon" href="/rocket.ico" />
-      </Head>
       <div>
-        <Navbar />
         <Header />
         <Card />
         <Form />
