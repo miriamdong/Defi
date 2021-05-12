dc9ff7b060efcf022f934098c3f620cc95e475e4
Miriam Dong <miriamdong@gmail.com>
2021-05-12 10:13:31 -0700

    fix create project

--- pages/project/create.jsx
+++ pages/project/create.jsx
@@ -423,13 +423,11 @@ export default function Create() {
                     className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Cancel
                   </button>
-                  <Link href='/'>
                   <button
                     type="submit"
                     className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Save
                   </button>
-                  </Link>
                 </div>
               </div>
             </form>
