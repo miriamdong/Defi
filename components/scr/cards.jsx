b3fa5f2dfdbb76a5d856742d9576ac9e3a4736fc
Miriam Dong <miriamdong@gmail.com>
2021-05-15 16:15:39 -0700

    add timer

--- components/scr/cards.jsx
+++ components/scr/cards.jsx
@@ -1,12 +1,12 @@
 /* This example requires Tailwind CSS v2.0+ */
 import Link from "next/link";
+import Fav from "../../components/fav";
 
-export default function C({projects,currentUser}) {
+export default function C({ projects, currentUser }) {
   return (
-    
-      <>
-        {projects.map((project) => (
-          <Link href="/project/[id]/story" as={`/project/${project.id}/story`}>
+    <>
+      {projects.map((project) => (
+        <Link href="/project/[id]/story" as={`/project/${project.id}/story`}>
           <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
             <div className="flex-shrink-0">
               <img className="h-48 w-full object-cover" src={project.image} alt="" />
@@ -19,39 +19,39 @@ export default function C({projects,currentUser}) {
                   </a>
                 </p>
                 <a href={project.link} className="block mt-2">
-                  <p className="text-xl font-semibold text-gray-900">{project.name}</p>
+                  <p className="text-xl font-semibold text-gray-900">
+                    {project.name} <Fav />
+                  </p>
                   <p className="mt-3 text-base text-gray-500">{project.description}</p>
                 </a>
               </div>
               <div className="pr-3 mt-6">
-              <div className="order-1 text-base leading-6 font-medium text-indigo-500 text-center">
-                Progressing
-              </div>         
-              <div className="text-right">
-                <span className="text-base font-semibold inline-block text-indigo-500">
-                  {project.funding}%
-                </span>
-              </div>
-              <div className="relative w-full ">
-                <div className=" overflow-hidden h-6 mb-4 text-xs flex rounded bg-purple-200">
-                  <div
-                    style={{ width: project.funding }}
-                    className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-purple-500"></div>
+                <div className="order-1 text-base leading-6 font-medium text-indigo-500 text-center">
+                  Progressing
+                </div>
+                <div className="text-right">
+                  <span className="text-base font-semibold inline-block text-indigo-500">
+                    {project.funding}%
+                  </span>
+                </div>
+                <div className="relative w-full ">
+                  <div className=" overflow-hidden h-6 mb-4 text-xs flex rounded bg-purple-200">
+                    <div
+                      style={{ width: project.funding }}
+                      className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-purple-500"></div>
+                  </div>
                 </div>
               </div>
-            </div>
               <div className="flex items-center">
-                
                 <div className="flex-shrink-0">
                   <a href={project.link}>
                     <span className="sr-only">NAME</span>
                     <img className="h-10 w-10 rounded-full" src={project.image} alt="" />
                   </a>
                 </div>
-                
+
                 <div className="ml-3">
                   <p className="text-sm font-medium text-gray-900">
-                    
                     <a href={project.link} className="hover:underline">
                       {project.username}
                     </a>
@@ -63,9 +63,8 @@ export default function C({projects,currentUser}) {
               </div>
             </div>
           </div>
-          </Link>
-        ))}
-     </>
-    
-  )
+        </Link>
+      ))}
+    </>
+  );
 }
