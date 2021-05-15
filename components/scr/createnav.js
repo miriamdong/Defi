cc3039b4abc54598c3ac0ef8a2fbda2fcb50ef9e
Miriam Dong <miriamdong@gmail.com>
2021-05-15 15:08:34 -0700

    bring in sharedwallet to create page

---
+++ components/scr/createnav.js
@@ -0,0 +1,5 @@
+export const tabs = [
+  { name: "KYC", href: "/create/KYC", current: false, slug: "kyc" },
+  { name: "Form", href: "/create/Form", current: false, slug: "form" },
+  { name: "Shared Wallet", href: "/create/SharedWallet", current: true, slug: "sharedwallet" },
+];
