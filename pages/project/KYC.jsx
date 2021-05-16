ace122c09e12b823e1a7a937b30edcccbf9388f4
Scarlet <scarletshao@gmail.com>
2021-05-16 12:11:43 -0700

    KYC

--- pages/project/KYC.jsx
+++ pages/project/KYC.jsx
@@ -10,7 +10,7 @@ import { firebaseClient } from "../../firebase/initFirebase";
 import { useRouter } from "next/router";
 import firebase from "firebase/app";
 import "firebase/auth";
-import Escrow from "../Escrow";
+import Escrow from "../../components/Meta/Escrow";
 import Createsteps from "./createsteps";
 import Kyc from "../../components/Meta/Kyc";
 
@@ -68,7 +68,7 @@ export default function Create() {
   console.log(state);
   return (
     <>
-      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 p-0">
+      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 p-40">
         <main className="bg-white">
           <div className="max-w-3xl mx-auto">
             <form className="space-y-8 divide-y divide-gray-200" onSubmit={handleSubmit}>
@@ -76,7 +76,7 @@ export default function Create() {
                 <div>
                   <div className="text-center">
                     <h1 className="text-6xl leading-6 font-medium text-gray-900 p-4">
-                      New Project
+                    Join Our KYC Program
                     </h1>
                     <p className="mt-10 mb-0 text-sm text-gray-500">
                       This information will be displayed publicly so be careful what you share.
@@ -85,16 +85,12 @@ export default function Create() {
                 </div>
                 <Createsteps steps={steps}/>
                 <div className="pt-8">
-                  <div>
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">
-                      Join Our KYC Program
-                    </h3>
-                  </div>
+                 
                   <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-                    <div className="sm:col-span-4">
+                    <div className="sm:col-span-6">
                       <label
                         htmlFor="Project_name"
-                        className="block text-sm font-medium text-gray-700">
+                        className="block text-lg font-medium text-gray-700">
                         My Wallet Address
                       </label>
                       <div className="mt-1">
