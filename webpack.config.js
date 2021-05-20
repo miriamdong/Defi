e9ba8f031919c3e3484f42d2be66c68f753c6b2d
Miriam Dong <miriamdong@gmail.com>
2021-05-19 19:23:29 -0700

    try one more time to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-import css from "file.css";
 
 module.exports = {
   entry: "./src/js/main.js",
