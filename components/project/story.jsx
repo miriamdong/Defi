1daaf4f988ee7c9592797a6f946af3b2e6276f5d
Scarlet <scarletshao@gmail.com>
2021-05-19 20:32:50 -0700

    story

--- components/project/story.jsx
+++ components/project/story.jsx
@@ -76,33 +76,33 @@ export default function Story() {
               Introducing
             </span>
             <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              JavaScript for Beginners
+            The outline of the progress
             </span>
           </h1>
           <p className="mt-8 text-xl text-gray-500 leading-8">
-            Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget
-            aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend
-            egestas fringilla sapien.
+          The new design project of an innovative graphene bulb based on technology patented by the National Academy of Sciences in Wrocław is now available. The technology implemented in the light bulb allows induction of light in the visible spectrum via the use of a laser beam with a wavelength in the infrared (IR) range. In the article, we will also share the market analysis and many interesting details about the project. The aim of the project is to create a prototype of a graphene bulb based on an original industrial design. 
           </p>
         </div>
         <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
           <p>
-            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed <strong>eget risus enim</strong>.
-            Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra
-            tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.{' '}
-            <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
+          The first stage of the graphene bulb design project consisted of finding <strong>technical solutions</strong> 
+          that would enable the construction of a new product in a patented technology, which thus far had not been used 
+          for commercial purposes. Specialists from various fields, including engineers specializing in laser technology, 
+          were involved in the project. The project turned out to be quite a <strong>challenge </strong>as it was impossible to use components 
+          available on the market. After constructing a dedicated laser and estimating the measurements of the target product, 
+          it was time for a concept design. 
           </p>
           <ul>
-            <li>Quis elit egestas venenatis mattis dignissim.</li>
-            <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+            <li>The light bulb's design</li>
+            <li>The main goal of the materials selection policy is to describe the list of materials that are necessary to manufacture a new product, along with the list of suppliers and the criteria for assessing the quality of materials and their application.</li>
+            <li>The presented materials policy is applicable for the production of individual light bulbs.</li>
           </ul>
           <p>
             Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam
             porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc
             purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.
           </p>
-          <h2>From beginner to expert in 30 days</h2>
+          <h2>Materials selection policy</h2>
           <p>
             Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
             Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus
@@ -111,18 +111,18 @@ export default function Story() {
           </p>
           <blockquote>
             <p>
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
+            Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
               pellentesque. Blandit amet, sed aenean erat arcu morbi.
             </p>
           </blockquote>
           <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
+          Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
             sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
           </p>
           <figure>
             <img
               className="w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
+              src="./img/light.jpeg"
               alt=""
               width={1310}
               height={873}
