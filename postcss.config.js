c720a1b4a5ce337427fc587202323d923accb1a5
Miriam Dong <miriamdong@gmail.com>
2021-05-19 20:18:51 -0700

    Tailwind come on

--- postcss.config.js
+++ postcss.config.js
@@ -6,12 +6,16 @@ const purgecss = [
   },
 ];
 
-module.exports = {
-  plugins: [
-    "postcss-preset-env",
-    "postcss-import",
-    "tailwindcss",
-    "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
-};
+module.exports = (ctx) => ({
+  parser: ctx.parser ? "sugarss" : false,
+  map: ctx.env === "development" ? ctx.map : false,
+  plugins: {
+    "postcss-import": {},
+    "postcss-nested": {},
+    "postcss-preset-env": {},
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: ctx.env === "production" ? {} : false,
+  },
+});
