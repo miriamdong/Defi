3aa0e0911b77ad5b75c79907feadbc4b8cce13f6
Miriam Dong <miriamdong@gmail.com>
2021-05-19 18:56:20 -0700

    add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+import css from "file.css";
 
 module.exports = {
   entry: "./src/js/main.js",
